00db9489354186dd1946280a1970bba508c88286
Monica Delgado <mnca.delgado@gmail.com>
2022-11-07 20:49:24 -1100

    added verifyInputField function

--- utils/calculate.js
+++ utils/calculate.js
@@ -1,29 +1,23 @@
-export const add = (first, second) => {
-  console.log("first", typeof first)
+function verifyInputField(first, second) {
   if (typeof first !== "number" || typeof second !== "number") {
     throw new Error("Enter only numbers")
   } else if (first === "" || second === "") {
     throw new Error("Monst enter a number")
   }
+}
+
+export const add = (first, second) => {
+  verifyInputField(first, second)
   return first + second
 }
 
 export const subtract = (first, second) => {
-  if (typeof first !== "number" || typeof second !== "number") {
-    throw new Error("Enter only numbers")
-  } else if (first === "" || second === "") {
-    throw new Error("Monst enter a number")
-  }
+  verifyInputField(first, second)
   return first - second
 }
 
 export const multiply = (first, second) => {
-  // if (typeof first !== "number" || typeof second !== "number") {
-  //   throw new Error("Enter only numbers")
-  // } else if (first === "" || second === "") {
-  //   throw new Error("Monst enter a number")
-  // }
-  // return first * second
+  //Fixes the api test error
   if (first === null || first === "" || second === null || second === "") {
     throw new Error("Must provide a number")
   }
@@ -34,11 +28,8 @@ export const multiply = (first, second) => {
 }
 
 export const divide = (first, second) => {
-  if (typeof first !== "number" || typeof second !== "number") {
-    throw new Error("Enter only numbers")
-  } else if (first === "" || second === "") {
-    throw new Error("Monst enter a number")
-  } else if (second === 0) {
+  verifyInputField(first, second)
+  if (second === 0) {
     throw new Error("Can't divide by zero")
   }
   return first / second
