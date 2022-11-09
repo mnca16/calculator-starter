10ad3dd60d6ac48770a6f22a0a2789f1aa12612b
Monica Delgado <mnca.delgado@gmail.com>
2022-11-08 15:06:10 -1100

    cleared code deleting examples

--- tests/api/calculate.spec.js
+++ tests/api/calculate.spec.js
@@ -1,53 +1,27 @@
 const { test, expect } = require("@playwright/test")
 
 test.describe("Basic API testing with valid input", () => {
-  test.only("simple addition", async ({ request }) => {
+  test("simple addition", async ({ request }) => {
     const result = await request.get("/api/calculate/add/2/2", {})
     expect(result.ok()).toBeTruthy()
     expect(await result.json()).toEqual({ result: 4 })
   })
 
-  test.only("simple subtaction", async ({ request }) => {
+  test("simple subtaction", async ({ request }) => {
     const result = await request.get("/api/calculate/subtract/6/3", {})
     expect(result.ok()).toBeTruthy()
     expect(await result.json()).toEqual({ result: 3 })
   })
 
-  test.only("simple multiplication", async ({ request }) => {
+  test("simple multiplication", async ({ request }) => {
     const result = await request.get("/api/calculate/multiply/6/3", {})
     expect(result.ok()).toBeTruthy()
     expect(await result.json()).toEqual({ result: 18 })
   })
 
-  test.only("simple division", async ({ request }) => {
+  test("simple division", async ({ request }) => {
     const result = await request.get("/api/calculate/divide/10/2", {})
     expect(result.ok()).toBeTruthy()
     expect(await result.json()).toEqual({ result: 5 })
   })
 })
-
-// test.describe("Basic API testing with invalid input", () => {
-//   test.only("simple addition", async ({ request }) => {
-//     const result = await request.get("/api/calculate/add/'2'/2", {})
-//     expect(result.ok()).toBeTruthy()
-//     expect(await result.json()).toEqual({ result: 4 })
-//   })
-
-//   test.only("simple subtaction", async ({ request }) => {
-//     const result = await request.get("/api/calculate/subtract/6/3", {})
-//     expect(result.ok()).toBeTruthy()
-//     expect(await result.json()).toEqual({ result: 3 })
-//   })
-
-//   test.only("simple multiplication", async ({ request }) => {
-//     const result = await request.get("/api/calculate/multiply/6/3", {})
-//     expect(result.ok()).toBeTruthy()
-//     expect(await result.json()).toEqual({ result: 18 })
-//   })
-
-//   test.only("simple division", async ({ request }) => {
-//     const result = await request.get("/api/calculate/divide/10/2", {})
-//     expect(result.ok()).toBeTruthy()
-//     expect(await result.json()).toEqual({ result: 5 })
-//   })
-// })
