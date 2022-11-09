ca33631898f8439caca3ed4b1f5d800ea9db42d0
Monica Delgado <mnca.delgado@gmail.com>
2022-11-08 19:24:50 -1100

    added home file to render home component

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,20 @@ export default function Home() {
   return (
     <Container maxWidth="sm">
       <Stack>
-        <Typography variant="h2" gutterBottom sx={{ marginBottom: "30px" }}>
+        <Typography
+          variant="h2"
+          data-testid="header-text"
+          gutterBottom
+          sx={{ marginBottom: "30px" }}
+        >
           The Amazing Calculator
         </Typography>
-        <Button variant="contained" type="button" sx={{ marginBottom: "30px" }}>
+        <Button
+          title="button"
+          variant="contained"
+          type="button"
+          sx={{ marginBottom: "30px" }}
+        >
           <Link href="/another-page">PAGE</Link>
         </Button>
         <Calculator />
