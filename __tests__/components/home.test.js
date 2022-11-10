97c53893e74ff23099475afff0697dd538ff3eb1
Monica Delgado <mnca.delgado@gmail.com>
2022-11-09 16:09:23 -1100

    deleted find <Calculator/> test - react library don't find child components

--- __tests__/components/home.test.js
+++ __tests__/components/home.test.js
@@ -4,13 +4,13 @@ import { render, screen } from "@testing-library/react"
 import Home from "../../pages/index.js"
 
 describe("Simple tests to grab elements of Home component with different query methods", () => {
-  it.only(" should render The Amazing Calculator text from Home component but text", () => {
+  it("should render The Amazing Calculator text from Home component", () => {
     render(<Home />)
     const typographyH2Element = screen.getByText(/The Amazing Calculator/i)
     expect(typographyH2Element).toBeInTheDocument()
   })
 
-  it.only("should render The Amazing Calculator text from Home component by role", () => {
+  it("should render The Amazing Calculator text from Home component by role", () => {
     render(<Home />)
     //when there is more than one element we add an object with the property name
     const typographyH2Element = screen.getByRole("heading", {
@@ -19,21 +19,15 @@ describe("Simple tests to grab elements of Home component with different query m
     expect(typographyH2Element).toBeInTheDocument()
   })
 
-  it.only("should render 'click me button' by tittle", () => {
+  it("should render 'click me button' by tittle", () => {
     render(<Home />)
     const clickeMeButton = screen.getByTitle("button")
     expect(clickeMeButton).toBeInTheDocument()
   })
 
-  it.only("should render The Amazing Calculator text from Home component by id", () => {
+  it("should render The Amazing Calculator text from Home component by id", () => {
     render(<Home />)
     const typographyWithId = screen.getByTestId("header-text")
     expect(typographyWithId).toBeInTheDocument()
-  })
-
-  it.only("should find the <calculator/> component", () => {
-    render(<Home />)
-    const typographyH1Element = screen.getByTestId("header-text")
-    expect(typographyH1Element).toBeInTheDocument()
   })
 })
