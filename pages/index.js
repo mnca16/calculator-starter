251d60e2c37a5bf340b2e87dc8a2ace5f6336468
Monica Delgado <mnca.delgado@gmail.com>
2022-11-11 18:23:16 -1100

    added div element to wrap home page elements and added Header components

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,28 @@
 import Link from "next/link"
 import { Typography, Container, Stack, Button } from "@mui/material"
 import Calculator from "../components/calculator"
+import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <Container maxWidth="sm">
-      <Stack>
-        <Typography
-          variant="h2"
-          data-testid="header-text"
-          gutterBottom
-          sx={{ marginBottom: "30px" }}
-        >
-          The Amazing Calculator
-        </Typography>
-        <Button
-          title="button"
-          variant="contained"
-          type="button"
-          sx={{ marginBottom: "30px" }}
-        >
-          <Link href="/another-page">PAGE</Link>
-        </Button>
-        <Calculator />
-      </Stack>
-    </Container>
+    <div>
+      <Header />
+      <Container
+        sx={{ border: "2px solid white ", marginTop: "1rem" }}
+        maxWidth="sm"
+      >
+        <Stack>
+          <Typography
+            variant="h2"
+            data-testid="header-text"
+            gutterBottom
+            sx={{ marginBottom: "30px" }}
+          >
+            The Amazing Calculator
+          </Typography>
+          <Calculator />
+        </Stack>
+      </Container>
+    </div>
   )
 }
