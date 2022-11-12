56aa6c7348038943cf62b0a27557090a391e5fea
Monica Delgado <mnca.delgado@gmail.com>
2022-11-11 18:24:42 -1100

    updated click me button with dog information

--- __tests__/components/anotherPage.test.js
+++ __tests__/components/anotherPage.test.js
@@ -5,16 +5,16 @@ import AnotherPage from "../../pages/another-page"
 describe("Another page elements", () => {
   it("Should render button", () => {
     render(<AnotherPage />)
-    const button = screen.getByRole("button")
+    const button = screen.getByRole("button", { name: "Pet me!" })
     expect(button).toBeInTheDocument()
   })
 
-  it("should render 'Hello There' when click on button", async () => {
+  it("should render 'Woof Woof!' when click on button", async () => {
     render(<AnotherPage />)
-    const buttonElement = screen.getByRole("button")
+    const buttonElement = screen.getByRole("button", { name: "Pet me!" })
     fireEvent.click(buttonElement)
 
     const typograpgyH2Element = await screen.findByTestId("text-id")
-    expect(typograpgyH2Element).toHaveTextContent("Hello There")
+    expect(typograpgyH2Element).toHaveTextContent("Woof Woof!")
   })
 })
