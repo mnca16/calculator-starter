c164a851611dedf400ea87df123e2fe01006f9bb
Monica Delgado <mnca.delgado@gmail.com>
2022-11-11 21:31:24 -1100

    fixed mock axios response

--- __tests__/components/anotherPage.test.js
+++ __tests__/components/anotherPage.test.js
@@ -1,19 +1,12 @@
 import "@testing-library/jest-dom"
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  cleanup,
-  act,
-} from "@testing-library/react"
+import { render, screen, fireEvent, cleanup, act } from "@testing-library/react"
 import AnotherPage from "../../pages/another-page"
 import axiosMock from "axios"
 
 afterEach(cleanup)
 
 describe("Another page elements", () => {
-  it("Should render button", () => {
+  it("Should render pet me button", () => {
     render(<AnotherPage />)
     const button = screen.getByRole("button", { name: "Pet me!" })
     expect(button).toBeInTheDocument()
@@ -31,18 +24,19 @@ describe("Another page elements", () => {
 
 describe("Mock axios API - dog images", () => {
   it("Should render dog's avatar everytime the page is refresh ", async () => {
-    const url = "/dog.ceo/api/breeds/image/random/4"
     axiosMock.get.mockResolvedValueOnce({
-      message: [
-        "https://images.dog.ceo/breeds/terrier-patterdale/dog-1268559_640.jpg",
-        "https://images.dog.ceo/breeds/entlebucher/n02108000_1844.jpg",
-        "https://images.dog.ceo/breeds/spaniel-irish/n02102973_4566.jpg",
-        "https://images.dog.ceo/breeds/mastiff-bull/n02108422_4793.jpg",
-      ],
-      status: "success",
+      data: {
+        message: [
+          "https://images.dog.ceo/breeds/terrier-patterdale/dog-1268559_640.jpg",
+          "https://images.dog.ceo/breeds/entlebucher/n02108000_1844.jpg",
+          "https://images.dog.ceo/breeds/spaniel-irish/n02102973_4566.jpg",
+          "https://images.dog.ceo/breeds/mastiff-bull/n02108422_4793.jpg",
+        ],
+        status: "success",
+      },
     })
 
-    await act(async () => await render(<AnotherPage />))
+    await act(async () => render(<AnotherPage />))
 
     expect(axiosMock.get).toHaveBeenCalledTimes(3)
   })
