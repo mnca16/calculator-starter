7d61aa4bce93cd2549bba76f17c284a1d29d56fb
Monica Delgado <mnca.delgado@gmail.com>
2022-11-11 18:22:01 -1100

    made fetch request to get dog avatar

--- pages/another-page.jsx
+++ pages/another-page.jsx
@@ -1,28 +1,58 @@
-import React, {useState} from 'react'
-import { Typography, Button, Container} from "@mui/material"
- 
+import React, {useState, useEffect} from 'react'
+import { Typography, Button, Container, Stack, Box} from "@mui/material"
+import Header from "../components/Header"
+import DogList from '../components/DogList';
+import axios from "axios";
+
 const AnotherPage = () => {
   const [showText, setShowText] = useState(false)
+  const [dogAvatar, setDogAvatar] = useState([]);
+  
+  const fetchDogAvatar = () => {
+    axios
+      .get(`https://dog.ceo/api/breeds/image/random/4`)
+      .then((res) => {
+        setDogAvatar(res.data.message);
+      })
+      .catch((err) => {
+        setDogAvatar(err.response.data.message);
+      });
+  };
+  
+  useEffect(() => {
+    fetchDogAvatar();
+  }, [])
+  
+
   const handleClick = () => {
     setShowText(!showText)
   }
-   return (
-    <Container maxWidth="sm">
+
+  return (
+    <>
+      <Header/>
+      <Container maxWidth="sm">
         <Typography id="title-page" variant="h2" gutterBottom sx={{ marginBottom: "30px" }}>
-          Another Page!
+          Calculator followers!
         </Typography>
+        <Stack direction="row" spacing={8}>
+        <DogList dogAvatar={dogAvatar}/>
+        <Box>
         <Button onClick={handleClick} variant="contained" type="submit" id="click-me" sx={{ marginBottom: "30px" }}>
-          Click me!
+          Pet me!
         </Button>
         {showText ? <DisplayText/> : null}
-   </Container>
+        </Box>
+        </Stack>
+    </Container>
+   </>
 )}
 
 const DisplayText = () => {
   return (
     <>
-      <Typography data-testid="text-id" id="show-results" variant="h2" gutterBottom sx={{ marginBottom: "30px" }}>
-       Hello There
+      <Typography data-testid="text-id" id="show-results" variant="h5" gutterBottom sx={{ marginBottom: "30px" }}>
+       Woof Woof!
       </Typography>
     </>
 )}
